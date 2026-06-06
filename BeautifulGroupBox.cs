5052c4173a79be6b99338f455d77d12345a682c2
bthu <bthu.fablab@gmail.com>
2026-06-07 00:23:17 +0700

    Add project files.

---
+++ BeautifulGroupBox.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace DLMT_BTL 
+{
+    public class BeautifulGroupBox : GroupBox
+    {
+        // Tạo một thuộc tính mới để bạn có thể đổi màu viền tùy thích
+        private Color borderColor = Color.DodgerBlue; // Đổi màu mặc định ở đây
+
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set { borderColor = value; this.Invalidate(); }
+        }
+
+        // Ép C# cho phép mình tự vẽ lại Control này
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Lấy kích thước chữ của tiêu đề
+            Size tSize = TextRenderer.MeasureText(this.Text, this.Font);
+            Rectangle borderRect = e.ClipRectangle;
+            borderRect.Y += tSize.Height / 2;
+            borderRect.Height -= tSize.Height / 2;
+
+            // 1. Vẽ cái viền mới với màu của mình
+            ControlPaint.DrawBorder(e.Graphics, borderRect, this.borderColor, ButtonBorderStyle.Solid);
+
+            // 2. Tạo một khoảng trống trên viền
+            Rectangle textRect = e.ClipRectangle;
+            textRect.X += 8; // Thụt lề chữ vào 8 pixel
+            textRect.Width = tSize.Width;
+            textRect.Height = tSize.Height;
+            e.Graphics.FillRectangle(new SolidBrush(this.BackColor), textRect);
+
+            // 3. In cái chữ Tiêu đề lên
+            e.Graphics.DrawString(this.Text, this.Font, new SolidBrush(this.ForeColor), textRect);
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace DLMT_BTL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.beautifulGroupBox1 = new DLMT_BTL.BeautifulGroupBox();
+            this.beautifulGroupBox2 = new DLMT_BTL.BeautifulGroupBox();
+            this.beautifulGroupBox3 = new DLMT_BTL.BeautifulGroupBox();
+            this.beautifulGroupBox4 = new DLMT_BTL.BeautifulGroupBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.beautifulGroupBox1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.beautifulGroupBox3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.beautifulGroupBox2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 181);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(930, 181);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.beautifulGroupBox4, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.beautifulGroupBox1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(930, 544);
+            this.tableLayoutPanel2.TabIndex = 7;
+            // 
+            // beautifulGroupBox1
+            // 
+            this.beautifulGroupBox1.BackColor = System.Drawing.Color.AliceBlue;
+            this.beautifulGroupBox1.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.beautifulGroupBox1.Controls.Add(this.tableLayoutPanel3);
+            this.beautifulGroupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.beautifulGroupBox1.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beautifulGroupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.beautifulGroupBox1.Location = new System.Drawing.Point(3, 3);
+            this.beautifulGroupBox1.Name = "beautifulGroupBox1";
+            this.beautifulGroupBox1.Size = new System.Drawing.Size(924, 175);
+            this.beautifulGroupBox1.TabIndex = 7;
+            this.beautifulGroupBox1.TabStop = false;
+            this.beautifulGroupBox1.Text = "Cấu hình phần cứng ";
+            // 
+            // beautifulGroupBox2
+            // 
+            this.beautifulGroupBox2.BackColor = System.Drawing.Color.AliceBlue;
+            this.beautifulGroupBox2.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.beautifulGroupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.beautifulGroupBox2.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beautifulGroupBox2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.beautifulGroupBox2.Location = new System.Drawing.Point(3, 3);
+            this.beautifulGroupBox2.Name = "beautifulGroupBox2";
+            this.beautifulGroupBox2.Size = new System.Drawing.Size(459, 175);
+            this.beautifulGroupBox2.TabIndex = 8;
+            this.beautifulGroupBox2.TabStop = false;
+            this.beautifulGroupBox2.Text = "Cấu hình mạng ";
+            // 
+            // beautifulGroupBox3
+            // 
+            this.beautifulGroupBox3.BackColor = System.Drawing.Color.AliceBlue;
+            this.beautifulGroupBox3.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.beautifulGroupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.beautifulGroupBox3.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beautifulGroupBox3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.beautifulGroupBox3.Location = new System.Drawing.Point(468, 3);
+            this.beautifulGroupBox3.Name = "beautifulGroupBox3";
+            this.beautifulGroupBox3.Size = new System.Drawing.Size(459, 175);
+            this.beautifulGroupBox3.TabIndex = 9;
+            this.beautifulGroupBox3.TabStop = false;
+            this.beautifulGroupBox3.Text = "Lưu giữ dữ liệu ";
+            // 
+            // beautifulGroupBox4
+            // 
+            this.beautifulGroupBox4.BackColor = System.Drawing.Color.AliceBlue;
+            this.beautifulGroupBox4.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.beautifulGroupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.beautifulGroupBox4.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beautifulGroupBox4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.beautifulGroupBox4.Location = new System.Drawing.Point(3, 365);
+            this.beautifulGroupBox4.Name = "beautifulGroupBox4";
+            this.beautifulGroupBox4.Size = new System.Drawing.Size(924, 176);
+            this.beautifulGroupBox4.TabIndex = 8;
+            this.beautifulGroupBox4.TabStop = false;
+            this.beautifulGroupBox4.Text = "Giám sát dữ liệu ";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.60784F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.28322F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.label3, 2, 1);
+            this.tableLayoutPanel3.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.maskedTextBox1, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 37);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(918, 135);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 67);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Vendor ID: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 13F);
+            this.label2.Location = new System.Drawing.Point(3, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 68);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Vendor ID: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(461, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(454, 61);
+            this.tableLayoutPanel4.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 13F);
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 13F);
+            this.button2.Location = new System.Drawing.Point(230, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(221, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(461, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(454, 68);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Trạng thái kết nối: ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.maskedTextBox1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(183, 15);
+            this.maskedTextBox1.Mask = "099.099.099.099";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(272, 36);
+            this.maskedTextBox1.TabIndex = 8;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(930, 544);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.beautifulGroupBox1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private BeautifulGroupBox beautifulGroupBox3;
+        private BeautifulGroupBox beautifulGroupBox2;
+        private BeautifulGroupBox beautifulGroupBox4;
+        private BeautifulGroupBox beautifulGroupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
